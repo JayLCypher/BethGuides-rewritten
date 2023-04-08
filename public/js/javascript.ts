5b4806dc7ab2d6e0cb11eceed63be5517212bc17
JayLCypher <john_l.c@hotmail.com>
2023-04-09 01:22:54 +0200

    added index page + tmr finished

--- public/js/javascript.ts
+++ public/js/javascript.ts
@@ -13,17 +13,19 @@ function transparentTitleBar() {
 }
 
 function keyboardPagination(e: KeyboardEvent) {
-	if (e.code === "ArrowRight") { window.open(document.getElementById("next")!.getAttribute("href")!,"_self") }
+	if (e.code === "ArrowRight") { window.open(document.getElementById("next")!.getAttribute("href")!, "_self") }
 	else if (e.code === "ArrowLeft") { window.open(document.getElementById("previous")!.getAttribute("href")!, "_self"); }
 }
 
 function sidenavActiveLink() {
-	const aList = Array.prototype.slice.call(document.getElementById("sidenavLeftList")!.getElementsByTagName("a")!, 0)!;
-	const aList2 = Array.prototype.slice.call(document.getElementById("sidenavLeftList2")!.getElementsByTagName("a")!, 0)!;
-	const aListFinal = Array.prototype.concat(aList, aList2);
+	const aList = Array.prototype.slice.call(document.getElementById("sidenavLeftList")?.getElementsByTagName("a")!, 0)!;
+	const aList2 = Array.prototype.slice.call(document.getElementById("sidenavLeftList2")?.getElementsByTagName("a")!, 0)!;
+	const aListFinal = Array.prototype.concat(aList, aList2)!;
+	if (!aListFinal) { return; }
 	const currentPath = window.location.pathname!;
 	for (const a of aListFinal) {
-		if (a.getAttribute("href")! == currentPath) {
+		const attr = a.getAttribute("href")!;
+		if (attr && attr == currentPath) {
 			a.classList.add("active");
 			break;
 		}
@@ -34,7 +36,7 @@ function sidenavActiveLink() {
 
 function sidenavRightBarHide() {
 	const sidenavRight = document.getElementById("sidenavRight")!;
-	if (!sidenavRight.querySelector("ul")!.querySelector("li")!) { sidenavRight.style.display = "none"; }
+	if (!sidenavRight.querySelector("ul")?.querySelector("li")!) { sidenavRight.style.display = "none"; }
 	document.removeEventListener("DOMContentLoaded", sidenavRightBarHide);
 }
 
@@ -44,3 +46,44 @@ function titleGlow() {
 }
 
 function toggleMenu(ID: string) { document.getElementById(ID)!.classList.toggle("show"); }
+
+// TODO: Test and implement this function for BOT, ask if it's a plan to do this for @all guides.
+function setBackgroundByTime() {
+	const date = new Date();
+	const hour = date.getHours();
+	var updateColor = false;
+
+	// set the background image based on the current time
+	if (hour >= 0 && hour < 7) { document.body.style.backgroundImage = "url('/img/backgrounds/img0.webp')"; }
+	else if (hour >= 7 && hour < 9) {
+		document.body.style.backgroundImage = "url('/img/backgrounds/img7.webp')";
+		updateColor = true;
+	}
+	else if (hour >= 9 && hour < 12) {
+		document.body.style.backgroundImage = "url('/img/backgrounds/img9.webp')";
+		updateColor = true;
+	}
+	else if (hour >= 12 && hour < 17) {
+		document.body.style.backgroundImage = "url('/img/backgrounds/img12.webp')";
+		updateColor = true;
+	}
+	else if (hour >= 17 && hour < 20) {
+		document.body.style.backgroundImage = "url('/img/backgrounds/img17.webp')";
+		updateColor = true;
+	}
+	else if (hour >= 20 && hour < 23) { document.body.style.backgroundImage = "url('/img/backgrounds/img20.webp')"; }
+	else { document.body.style.backgroundImage = "url('/img/backgrounds/img23.webp')"; }
+
+	// TODO
+	// Could probs just force a color theme change
+	if (updateColor) {
+		const headerTitle = document.getElementById('headerTitle');
+		const button = document.getElementById('sideButton');
+		const headerLinks = document.getElementById('headerLinks');
+		button!.style.color = "rgba(0, 0, 0, 0.75)";
+		headerTitle!.style.color = "rgba(0, 0, 0, 0.75)";
+		Array.from(headerLinks!.children).forEach(child => {
+			child.setAttribute("style", "color: rgba(0, 0, 0, 0.75);");
+		});
+	}
+}
