5f8c11bd62f37482688d48940bd28ad33dab6589
JayLCypher <john_l.c@hotmail.com>
2023-04-10 02:57:26 +0200

    alright, that's the tweaks for now.

--- source/ddragonbornsfate.d
+++ source/ddragonbornsfate.d
@@ -40,4 +40,28 @@ class DDragonbornsFateController {
 		next = "/adf/utilities";
 		render!("adf/mo2.dt", title, shorttitle, subtitle, previous, next, github, description);
 	}
+
+	void getUtilities() {
+		description = "Utilities section full of mods that do some extra function or are required by other mods.";
+		subtitle = "Utilities";
+		previous = "/adf/mo2";
+		next = "/adf/bugfix";
+		render!("adf/utilities.dt", title, shorttitle, subtitle, previous, next, github, description);
+	}
+
+	void getBugfix() {
+		description = "Essential mods that fix bugs, improve performance, and/or improve quality of life.";
+		subtitle = "Bug Fixes";
+		previous = "/adf/utilities";
+		next = "/adf/tweaks";
+		render!("adf/bugfix.dt", title, shorttitle, subtitle, previous, next, github, description);
+	}
+
+	void getTweaks() {
+		description = "Essential mods that fix bugs, improve performance, and/or improve quality of life.";
+		subtitle = "Bug Fixes";
+		previous = "/adf/bugfix";
+		next = "/adf/hud";
+		render!("adf/tweaks.dt", title, shorttitle, subtitle, previous, next, github, description);
+	}
 }
